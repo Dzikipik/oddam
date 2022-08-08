347c1a1064d8b65e7fad7650ed8367cef867a32f
Karola Glowacz <d3ikipik@gmail.com>
2022-08-08 11:58:15 +0200

    styling hero section

---
+++ src/components/HeroIntro.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HeroIntro() {
+    return (
+      <div className="herointro">
+        Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce
+      </div>
+    );
+  }
--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Navigation from "./Navigation";
+import HeroImage from "./HeroImage";
+import HeroIntro from "./HeroIntro";
 
 export default function HomeHeader() {
   return (
     <header id="start" className="homeheader">
-      Header
+      <Navigation />
+      <HeroImage />
+      <HeroIntro />
     </header>
   );
 }
