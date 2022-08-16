24ff63ef4c9d07ba465654f0df492a9222f6a942
Karola Glowacz <d3ikipik@gmail.com>
2022-08-16 16:26:53 +0200

    added a little bit of hero and navigation styling

--- src/components/HeroIntro.tsx
+++ src/components/HeroIntro.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 export default function HeroIntro() {
     return (
       <div className="herointro">
-        Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce
+        <div className="herointro-title">
+          Zacznij pomagać!
+          Oddaj niechciane rzeczy w zaufane ręce
+        </div>
+        <div className="herointro-btns">
+          <div className="herointro-btn">Oddaj rzeczy</div>
+          <div className="herointro-btn">Zorganizuj zbiórkę</div>
+        </div>
       </div>
     );
   }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default function Navigation() {
     return (
+      <>
       <div className="navigation">
-        Nawigacja
+      <div className="navigation-loginPanel">
+        <li className="navigation-li">Zaloguj</li>
+        <li className="navigation-li">Załóż konto</li>
       </div>
+        <div className="navigation-menu">
+          <li className="navigation-li">Start</li>
+          <li className="navigation-li">O co chodzi?</li>
+          <li className="navigation-li">O nas</li>
+          <li className="navigation-li">Fundacja i organizacje</li>
+          <li className="navigation-li">Kontakt</li>
+        </div>
+      </div>
+      </>
     );
   }
