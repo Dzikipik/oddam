b5c6c75072a1154cab9945f5e06700dd7c45b1b5
Karola Glowacz <d3ikipik@gmail.com>
2022-08-16 18:53:23 +0200

    add svg module

--- src/components/HeroIntro.tsx
+++ src/components/HeroIntro.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import decor from "../assets/Decoration.svg";
 
 export default function HeroIntro() {
+  
     return (
       <div className="herointro">
         <div className="herointro-title">
           Zacznij pomagać!
           Oddaj niechciane rzeczy w zaufane ręce
         </div>
+        <img src={decor} alt="" />
         <div className="herointro-btns">
           <div className="herointro-btn">Oddaj rzeczy</div>
           <div className="herointro-btn">Zorganizuj zbiórkę</div>
